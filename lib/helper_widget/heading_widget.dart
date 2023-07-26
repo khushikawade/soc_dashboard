bf2c60c39fe5fc738fd758f61d5cae3d99634406
ztsy0009 <sayyam.yadav@zehntech.com>
2023-07-26 19:42:07 +0530

    changes the font

--- lib/helper_widget/heading_widget.dart
+++ lib/helper_widget/heading_widget.dart
@@ -55,7 +55,7 @@ Widget detailTextWidget(text, context) {
         color: AppColors.black,
         fontWeight: FontWeight.w400,
         fontSize: AppSize.size16,
-        fontFamily: 'Inter',
+        fontFamily: 'Poppins',
         fontStyle: FontStyle.normal,
       ));
 }

cc24b95aefd95e40a2e272a4805c064eacdea88f
ztsy0009 <sayyam.yadav@zehntech.com>
2023-07-27 11:12:54 +0530

    changes done

--- lib/screen_ui/home_module/home.dart
+++ lib/screen_ui/home_module/home.dart
@@ -43,6 +43,12 @@ class _HomeState extends State<Home> {
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut placerat libero. In sed lorem at turpis cursus imperdiet nec at dui. Aenean tortor mauris, consequat non imperdiet vel, tristique nec urna.",
             imageUrlCard1:
                 "https://s3-alpha-sig.figma.com/img/d78c/cb1f/2bedc8d8bf4cfce811469c0e9d05c7f8?Expires=1691366400&Signature=et-U8SEdeh41abh31WGCOLuvCZZ42ZqDyZhNiRxt2NKS8MY3H9oi7fXhXER8DSJBHRqNKStRrlFpi-MQdRVtjhr4jxHxBRjwTtov6zT0UTRE8dwuDDvGkVsFJgYemxyEB1IGQTiwTwxc6kjxse~JZLIJhFaFadDaXXb8guSdexF7Y4pioOrezkcXEzGZJ6UT9aki-xEFe1u5uEJk6BEG5851AkO2tDYHAQimuf5zePiJhNhGxifQDTy3LQM7KmlzCoavAZ~dQ1sD7YimIg7Q1hi846Deg7oi0QZRER31cJtCHg6uY8YPpRRqDciQt7Ddw2q9nMrdKM5m1Rr1lyx-Ng__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+            titleTextCard2: "Title Here",
+            subTitleTextCard2: "Praesent lobortis metus ex.",
+            detailDescriptionCard2:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut placerat libero. In sed lorem at turpis cursus imperdiet nec at dui. Aenean tortor mauris, consequat non imperdiet vel, tristique nec urna.",
+            imageUrlCard2:
+                "https://s3-alpha-sig.figma.com/img/d78c/cb1f/2bedc8d8bf4cfce811469c0e9d05c7f8?Expires=1691366400&Signature=et-U8SEdeh41abh31WGCOLuvCZZ42ZqDyZhNiRxt2NKS8MY3H9oi7fXhXER8DSJBHRqNKStRrlFpi-MQdRVtjhr4jxHxBRjwTtov6zT0UTRE8dwuDDvGkVsFJgYemxyEB1IGQTiwTwxc6kjxse~JZLIJhFaFadDaXXb8guSdexF7Y4pioOrezkcXEzGZJ6UT9aki-xEFe1u5uEJk6BEG5851AkO2tDYHAQimuf5zePiJhNhGxifQDTy3LQM7KmlzCoavAZ~dQ1sD7YimIg7Q1hi846Deg7oi0QZRER31cJtCHg6uY8YPpRRqDciQt7Ddw2q9nMrdKM5m1Rr1lyx-Ng__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
           )
         ],
       ),

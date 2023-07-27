05443f6e2ce96da480abdd3f7bf8c98316dce3d1
ztsy0009 <sayyam.yadav@zehntech.com>
2023-07-27 13:18:47 +0530

    change..

--- lib/helper_widget/common_widget/one_column_text_and_image_left.dart
+++ lib/helper_widget/common_widget/one_column_text_and_image_left.dart
@@ -28,67 +28,58 @@ class _OneColumnTextAndImageLeftWidgetState
     extends State<OneColumnTextAndImageLeftWidget> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-          left: 191.w,
-          right: 191.w,
-          bottom: 105.h,
-          top: MediaQuery.of(context).size.height / 6),
-      child: Card(
-        elevation: 2,
+    return Container(
+      padding:
+          EdgeInsets.only(left: 40.w, top: 55.h, bottom: 90.h, right: 55.w),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10.r)),
+        boxShadow: const [
+          BoxShadow(
+              color: Color.fromARGB(
+            0,
+            0,
+            0,
+            0,
+          ))
+        ],
         color: AppColors.whiteColor,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 52.w, top: 55.h),
-              child: headerTextWidget(widget.titleText, context),
-            ),
-            // VerticalSpacerWidget(53.h),
-            Padding(
-              padding: EdgeInsets.only(
-                left: 52.w,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: EdgeInsets.only(right: 52.w),
-                      child: CustomImage(
-                        // height: 400,
-                        // width: 300,
-                        iconUrl: widget.imageUrl,
-                      ),
-                    ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          headerTextWidget(widget.titleText, context),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: EdgeInsets.only(right: 52.w, top: 53.h),
+                  child: CustomImage(
+                    // height: 400,
+                    // width: 300,
+                    iconUrl: widget.imageUrl,
                   ),
-                  Expanded(
-                    flex: 2,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(top: 53.h),
-                          child:
-                              subHeaderTextWidget(widget.subTitleText, context),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(
-                              bottom: 90.h, top: 30.h, right: 52.w),
-                          child: detailTextWidget(
-                              widget.detailDescription, context),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ],
-        ),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    subHeaderTextWidget(widget.subTitleText, context),
+                    SizedBox(
+                      height: 30.h,
+                    ),
+                    detailTextWidget(widget.detailDescription, context),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }

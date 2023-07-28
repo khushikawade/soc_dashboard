bd6b988d62e0b2afe8dccea0cb29aaa4b0e394c4
ztsy0009 <sayyam.yadav@zehntech.com>
2023-07-28 16:32:55 +0530

    tested the flutter html

--- lib/helper_widget/heading_widget.dart
+++ lib/helper_widget/heading_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:solved_dashboard/utils/app_size.dart';
 import 'package:solved_dashboard/utils/app_theme.dart';
+import 'package:solved_dashboard/utils/app_util.dart';
 
 import '../utils/app_colors.dart';
 
@@ -37,5 +39,36 @@ Widget subHeaderTextWidget(text, context) {
 }
 
 Widget detailTextWidget(text, context) {
-  return Text(text, style: AppTheme.lightTheme.textTheme.labelSmall);
+  //return Text(text, style: AppTheme.lightTheme.textTheme.labelSmall);
+
+  return Html(
+    data: "$text",
+    shrinkWrap: false,
+    onLinkTap: (url, attributes, element) {
+      AppUtil.urlLauncher(url);
+    },
+    extensions: [
+      TagExtension(tagsToExtend: {"flutter"}, child: const SizedBox.shrink()),
+    ],
+    style: {
+      "body": Style(
+        margin: Margins.zero,
+        // padding: HtmlPaddings.zero,
+        color: AppColors.black,
+        fontWeight: FontWeight.w400,
+        fontSize: FontSize(AppSize.size16),
+        fontFamily: 'Poppins',
+        fontStyle: FontStyle.normal,
+      ),
+      "p": Style(
+        margin: Margins.zero,
+        // padding: HtmlPaddings.zero,
+        color: AppColors.black,
+        fontWeight: FontWeight.w400,
+        fontSize: FontSize(AppSize.size16),
+        fontFamily: 'Poppins',
+        fontStyle: FontStyle.normal,
+      )
+    },
+  );
 }
--- lib/screen_ui/home_module/home.dart
+++ lib/screen_ui/home_module/home.dart
@@ -38,18 +38,36 @@ class _HomeState extends State<Home> {
           const ThreeColumnTextAndImageWidget(
             titleTextCard1: "Title Here",
             subTitleTextCard1: "There are Variations.",
-            detailDescriptionCard1:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut placerat libero. In sed lorem at turpis cursus imperdiet nec at dui. Aenean tortor mauris, consequat non imperdiet vel, tristique nec urna.",
+            detailDescriptionCard1: '''<div>
+    <p>
+      - Marking Period 1 & 2 -ELA, MATH, Science, Social Studies <br>
+      - Available in <a href="https://lookerstudio.google.com/u/0/reporting/75743c2d-5749-45e7-9562-58d0928662b2/page/m87aB">Student Reports</a> &amp; <a href="https://lookerstudio.google.com/u/0/reporting/75743c2d-5749-45e7-9562-58d0928662b2/page/p_9grki1pw2c">Report Card</a>
+    
+    </p>
+  </div>
+''',
             imageUrlCard1:
                 "https://s3-alpha-sig.figma.com/img/d78c/cb1f/2bedc8d8bf4cfce811469c0e9d05c7f8?Expires=1691366400&Signature=et-U8SEdeh41abh31WGCOLuvCZZ42ZqDyZhNiRxt2NKS8MY3H9oi7fXhXER8DSJBHRqNKStRrlFpi-MQdRVtjhr4jxHxBRjwTtov6zT0UTRE8dwuDDvGkVsFJgYemxyEB1IGQTiwTwxc6kjxse~JZLIJhFaFadDaXXb8guSdexF7Y4pioOrezkcXEzGZJ6UT9aki-xEFe1u5uEJk6BEG5851AkO2tDYHAQimuf5zePiJhNhGxifQDTy3LQM7KmlzCoavAZ~dQ1sD7YimIg7Q1hi846Deg7oi0QZRER31cJtCHg6uY8YPpRRqDciQt7Ddw2q9nMrdKM5m1Rr1lyx-Ng__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
             subTitleTextCard2: "Praesent lobor",
-            detailDescriptionCard2:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut placerat libero. In sed lorem at turpis cursus imperdiet nec at dui. Aenean tortor mauris, consequat non imperdiet vel, tristique nec urna.",
+            detailDescriptionCard2: '''<div>
+    <p>
+      - Marking Period 1 & 2 -ELA, MATH, Science, Social Studies <br>
+      - Available in <a href="https://lookerstudio.google.com/u/0/reporting/75743c2d-5749-45e7-9562-58d0928662b2/page/m87aB">Student Reports</a> &amp; <a href="https://lookerstudio.google.com/u/0/reporting/75743c2d-5749-45e7-9562-58d0928662b2/page/p_9grki1pw2c">Report Card</a>
+    
+    </p>
+  </div>
+''',
             imageUrlCard2:
                 "https://s3-alpha-sig.figma.com/img/d78c/cb1f/2bedc8d8bf4cfce811469c0e9d05c7f8?Expires=1691366400&Signature=et-U8SEdeh41abh31WGCOLuvCZZ42ZqDyZhNiRxt2NKS8MY3H9oi7fXhXER8DSJBHRqNKStRrlFpi-MQdRVtjhr4jxHxBRjwTtov6zT0UTRE8dwuDDvGkVsFJgYemxyEB1IGQTiwTwxc6kjxse~JZLIJhFaFadDaXXb8guSdexF7Y4pioOrezkcXEzGZJ6UT9aki-xEFe1u5uEJk6BEG5851AkO2tDYHAQimuf5zePiJhNhGxifQDTy3LQM7KmlzCoavAZ~dQ1sD7YimIg7Q1hi846Deg7oi0QZRER31cJtCHg6uY8YPpRRqDciQt7Ddw2q9nMrdKM5m1Rr1lyx-Ng__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
             subTitleTextCard3: "There are Variations.",
-            detailDescriptionCard3:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut placerat libero. In sed lorem at turpis cursus imperdiet nec at dui. Aenean tortor mauris, consequat non imperdiet vel, tristique nec urna.",
+            detailDescriptionCard3: '''<div>
+    <p>
+      - Marking Period 1 & 2 -ELA, MATH, Science, Social Studies <br>
+      - Available in <a href="https://lookerstudio.google.com/u/0/reporting/75743c2d-5749-45e7-9562-58d0928662b2/page/m87aB">Student Reports</a> &amp; <a href="https://lookerstudio.google.com/u/0/reporting/75743c2d-5749-45e7-9562-58d0928662b2/page/p_9grki1pw2c">Report Card</a>
+    
+    </p>
+  </div>
+''',
             imageUrlCard3:
                 "https://s3-alpha-sig.figma.com/img/d78c/cb1f/2bedc8d8bf4cfce811469c0e9d05c7f8?Expires=1691366400&Signature=et-U8SEdeh41abh31WGCOLuvCZZ42ZqDyZhNiRxt2NKS8MY3H9oi7fXhXER8DSJBHRqNKStRrlFpi-MQdRVtjhr4jxHxBRjwTtov6zT0UTRE8dwuDDvGkVsFJgYemxyEB1IGQTiwTwxc6kjxse~JZLIJhFaFadDaXXb8guSdexF7Y4pioOrezkcXEzGZJ6UT9aki-xEFe1u5uEJk6BEG5851AkO2tDYHAQimuf5zePiJhNhGxifQDTy3LQM7KmlzCoavAZ~dQ1sD7YimIg7Q1hi846Deg7oi0QZRER31cJtCHg6uY8YPpRRqDciQt7Ddw2q9nMrdKM5m1Rr1lyx-Ng__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
           )

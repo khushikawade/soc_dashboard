3f14cd760b8549e56f3ac5434a34b1dc9e68c236
ztsy0009 <sayyam.yadav@zehntech.com>
2023-08-03 13:07:34 +0530

    changes in faq done

--- lib/helper_widget/common_widget/push_notifcation_card.dart
+++ lib/helper_widget/common_widget/push_notifcation_card.dart
@@ -50,31 +50,33 @@ class _PushNotificationCustomCardState
     extends State<PushNotificationCustomCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 72.h, bottom: 71.w),
-      color: AppColors.backgroundBlueColor,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          _buildCardWidget(context, widget.icon1, widget.notificationText1!,
-              widget.subNotificationText1!),
-          SizedBox(
-            width: 34.5.w,
-          ),
-          _buildCardWidget(context, widget.icon2, widget.notificationText2!,
-              widget.subNotificationText2!),
-          SizedBox(
-            width: 34.5.w,
-          ),
-          _buildCardWidget(context, widget.icon3, widget.notificationText4!,
-              widget.subNotificationText3!),
-          SizedBox(
-            width: 34.5.w,
-          ),
-          _buildCardWidget(context, widget.icon4, widget.notificationText4!,
-              widget.subNotificationText4!),
-        ],
+    return SelectionArea(
+      child: Container(
+        padding: EdgeInsets.only(top: 72.h, bottom: 71.w),
+        color: AppColors.backgroundBlueColor,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _buildCardWidget(context, widget.icon1, widget.notificationText1!,
+                widget.subNotificationText1!),
+            SizedBox(
+              width: 34.5.w,
+            ),
+            _buildCardWidget(context, widget.icon2, widget.notificationText2!,
+                widget.subNotificationText2!),
+            SizedBox(
+              width: 34.5.w,
+            ),
+            _buildCardWidget(context, widget.icon3, widget.notificationText4!,
+                widget.subNotificationText3!),
+            SizedBox(
+              width: 34.5.w,
+            ),
+            _buildCardWidget(context, widget.icon4, widget.notificationText4!,
+                widget.subNotificationText4!),
+          ],
+        ),
       ),
     );
   }

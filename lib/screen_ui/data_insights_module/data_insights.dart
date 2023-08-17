a5bff9c81e6b5ea1b2ca7ca88f4082e0d5810757
ztsy0009 <sayyam.yadav@zehntech.com>
2023-08-17 17:38:45 +0530

    add iframe in data insight

--- lib/screen_ui/data_insights_module/data_insights.dart
+++ lib/screen_ui/data_insights_module/data_insights.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
+import 'package:solved_dashboard/helper_widget/common_widget/iframe_custom_link_widget.dart';
 import 'package:solved_dashboard/helper_widget/copy_right_widget.dart';
 import 'package:solved_dashboard/helper_widget/heading_widget.dart';
 import 'package:solved_dashboard/helper_widget/loading_widget.dart';
@@ -36,25 +37,51 @@ class _DataInsightsState extends State<DataInsights> {
         ? loadingWidget(context)
         : Column(
             children: [
-              ListView(
-                shrinkWrap: true,
-                physics: const BouncingScrollPhysics(),
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: 190.sp, right: 190.sp, top: 40.sp),
-                    child: pageTitleTextWidget(
-                        dataInsightsViewModel.homeDetails!.titleC != null &&
-                                dataInsightsViewModel
-                                    .homeDetails!.titleC!.isNotEmpty
-                            ? dataInsightsViewModel.homeDetails!.titleC
-                            : 'Data Insights',
-                        context),
-                  ),
-                  SizedBox(
-                    height: 40.h,
-                  ),
-                ],
+              Expanded(
+                child: ListView(
+                  shrinkWrap: true,
+                  physics: const BouncingScrollPhysics(),
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: 190.sp, right: 190.sp, top: 40.sp),
+                      child: pageTitleTextWidget(
+                          dataInsightsViewModel.homeDetails!.titleC != null &&
+                                  dataInsightsViewModel
+                                      .homeDetails!.titleC!.isNotEmpty
+                              ? dataInsightsViewModel.homeDetails!.titleC
+                              : 'Data Insights',
+                          context),
+                    ),
+                    SizedBox(
+                      height: 40.h,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: 190.sp,
+                        right: 190.sp,
+                      ),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: MediaQuery.of(context).size.height / 1.5.h,
+                        child: IFrameCustomWidget(
+                          titleText: "",
+                          subTitleText: "",
+                          dynamicLinkUrl:
+                              // dataInsightsViewModel.homeDetails !=
+                              //         null
+                              //     ? dataInsightsViewModel.homeDetails!.embedUrlC
+                              //         .toString()
+                              //     :
+                              "https://services.solvedconsulting.net/news/a1f4W000007DQaNQAW",
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40.h,
+                    ),
+                  ],
+                ),
               ),
               copyRightWidget(
                 '© 2023 Bronx Bears. All Rights Reserved.',
